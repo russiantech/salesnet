5a68328e6527001c30548cf8894b57d69fa9e4ca
russiantech <chrisjsmez@gmail.com>
2025-02-04 21:14:54 +0100

    redis fixed for different servers[production&development]

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from os import getenv
 from web import create_app
 
-app = create_app('development')  # Set to 'production' if needed
-# app = create_app('production')  # Set to 'production' if needed
+# app = create_app('development')  # Set to 'production' if needed
+app = create_app('production')  # Set to 'production' if needed
 print(app.config['REDIS_URL'])
 
 if __name__ == "__main__":
